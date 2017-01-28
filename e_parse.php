2752fc6171b1f9aec37a361cc5aebda99aaeb5a5
Arun S. Sekher <arunshekher@users.noreply.github.com>
2017-01-29 03:18:09 +0400

    Add class properties, add PHP Html DOM Parser method, add setter methods
    This commit add plugin prefs, status, ignore domain, ignore pages, properties,
    add two setter methods for the first two properties, add and test a method that
    uses PHP HTML DOM Parser to insert rel="nofollow" to anchor tags. Compare the
    functionality of REGEX and HTML DOM Parser methods with e107 benchmark class.
    	* Add $_nofollow_Prefs, $_nofollow_Active, $_nofollow_ignoreDomains, $_nofollow_ignorePages
    	* Add Prefs(), Status() static methods
    	* Add an experimental Html DOM Parser Method

--- e_parse.php
+++ e_parse.php
@@ -21,38 +21,82 @@ if (!defined('e107_INIT')) { exit; }
 
 class nofollow_parse
 {
+    
+    
+        private static $_nofollow_Prefs = array();
+        private static $_nofollow_Active = false;
+        private $_nofollow_ignoreDomains = array();
+        private $_nofollow_ignorePages = array();
 
 
-	/* constructor */
+        /* constructor */
 	function __construct()
 	{
+            // admin area doesnt require nofollow service
             if(e_ADMIN_AREA === true) 
             { 
                 return; 
             }
+            // set prefs
+            self::Prefs();
+            // set status
+            self::Status();
+        }
+        
+        
+        /**
+         * Pref Setter - Retrieve and set plugin preferences
+         * 
+         */
+        protected static function Prefs()
+        {
+            self::$_nofollow_Prefs = e107::getPlugPref('nofollow');
+        }
+        
+        
+        /**
+         * Status Setter - set plugin status
+         * 
+         */
+        protected static function Status()
+        {
+            if ( count(self::$_nofollow_Prefs ) )
+            {
+                self::$_nofollow_Active = self::$_nofollow_Prefs['globally_on'];
+            }
+            else
+            {
+                self::$_nofollow_Active = false;
+            }
+        }
 
-	}
-
-
-	/**
+        /**
 	 * @param string $text html/text to be processed.
 	 * @param string $context Current context ie.  OLDDEFAULT | BODY | TITLE | SUMMARY | DESCRIPTION | WYSIWYG etc.
 	 * @return string
 	 */
 	function toHtml($text, $context='')
 	{
-            //$text = str_replace('****', '<hr />', $text);
-            
-            
-            //$text = $this->makeNoFollow($text);
+            //require_once e_HANDLER.'benchmark.php';
+            //$bench = new e_benchmark();
+            //$bench->start();
             
             $text = $this->nofollow_toHtml($text);
+            
+            //$text = $this->nofollow_toHtml_DOM( $text );
+            
+            //$bench->end()->logResult('Nofollow_DOM_Method-1');
+            //$bench->end()->logResult('Nofollow_REGEX_Method-1');
+            //$bench->end();
+            //$bench->printResult();
+            // Debug
+            //print_a(self::$_nofollow_Active);
             return $text;
 	}
         
         
         
-    /**
+        /**
 	 * Adds rel="nofollow" attribute to html anchor tags if not present.
 	 * If already have an rel attr. but no nofollow value, appends nofollow. 
 	 * Inserts rel="nofollow" for everything else passed to it.
@@ -65,6 +109,7 @@ class nofollow_parse
          * in anchor tag and replace with rel="external nofollow" target="_blank" 
          * because appending nofollow to an rel attribute with external vaue will 
          * break e107's JS way of making the link open in new window
+         * @todo may be refactor the name to 'insert_Nofollow' or 'add_Nofollow'
 	 */
 	protected function stamp_NoFollow($anchor)
 	{
@@ -90,7 +135,7 @@ class nofollow_parse
 	
 	/**
 	 * Split up $text by html tags scans for anchor tags and apply 
-     * nofollow to suitable anchor tag candidates
+         * nofollow to suitable anchor tag candidates
 	 * (adopted from linkwords plugin.)
 	 * 
 	 * @param str $text - text string that will be altered
@@ -123,6 +168,69 @@ class nofollow_parse
                   }
               }
               return $nf_text;
+          }
+          
+          
+          
+          /**
+           * Alternative method to add nofollow using PHP HTML DOM Parser
+           * Has slight temporal edge over the REGEX method when tested with e107 benchmark class
+           * 
+           * @todo Have a known consequence of adding some unprintable space 
+           * character or something that mess up bootstrap styling little bit, 
+           * to be precise causing the caret sign to drop to a new line in some anchor tags.
+           * Update: Found out that its(probably the foreach loop which iterates anchor tags) 
+           * stripping 'style' attribute values from anchor tags.
+           * 
+           * @param type $text
+           * @return type
+           */
+          protected function nofollow_toHtml_DOM( $text )
+          {
+                $dom = new DOMDocument;
+ 
+                $dom->loadHTML( $text );
+
+                $anchors = $dom->getElementsByTagName( 'a' );
+
+                foreach( $anchors as $anchor )
+                { 
+                    $rel = array(); 
+
+                    if ( $anchor->hasAttribute( 'rel' ) AND ( $relAtt = $anchor->getAttribute( 'rel' ) ) !== '' )
+                    {
+                       $rel = preg_split( '/\s+/', trim($relAtt) );
+                    }
+
+                    if ( in_array( 'nofollow', $rel ) )
+                    {
+                      continue;
+                    }
+                    
+                    if ( in_array( 'external', $rel ) )
+                    {
+                        $anchor->setAttribute( 'target', '_blank');
+                    }
+
+                    $rel[] = 'nofollow';
+                    $anchor->setAttribute( 'rel', implode( ' ', $rel ) );
+                }
+
+                $dom->saveHTML();
+
+                $html = '';
+
+                foreach( $dom->getElementsByTagName( 'body' )->item( 0 )->childNodes as $element ) {
+                    $html .= $dom->saveXML( $element, LIBXML_NOEMPTYTAG );
+                }
+
+                return $html;      
+          }
+          
+          
+          protected static function goodDomain( $domain )
+          {
+              
           }
 
 
